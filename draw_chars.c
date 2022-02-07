75cfb167528b33535175194bb46cb209655d4178
Jonathan-Avalos <javalos11@miners.utep.edu>
2022-02-07 11:12:29 -0700

    added arrow and new font

--- draw_chars.c
+++ draw_chars.c
@@ -4,11 +4,23 @@
 void print_char_11x16(char c)
 {
   c -= 0x20;
-  for (char row = 0; row < 11; row++) {
-    unsigned short rowBits = font_11x16[c][row];
-    for (char col = 0; col < 16; col++) {
-      unsigned short colMask = 1 << (15-col); /* mask to select bit associated with bit */
+  for (char row = 0; row < 16; row++) {
+    unsigned short rowBits = 1 << (row);
+    for (char col = 0; col < 11; col++) {
+      unsigned short colMask = font_11x16[c][col]; /* mask to select bit associated with bit */
       putchar( (rowBits & colMask) ? '*' : ' ');
+    }
+    putchar('\n');
+  }
+}
+void print_char_8x12(char c)
+{
+  c -= 0x20;
+  for (char col = 0; col < 12; col++) {
+    unsigned short colMask = font_8x12[c][col];
+    for (char row = 0; row < 8; row++) {
+      unsigned short rowBits = 1 << (7-row);
+      putchar( (rowBits & colMask) ? ' ' : '*');
     }
     putchar('\n');
   }
